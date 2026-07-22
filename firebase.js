e1ec7032b1a8737feea04cbcd298b315b8d3dc1d
anonc137 <anonc137@proton.me>
2026-07-22 23:01:54 +0100

    dashboard

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtYffsXI4nlta8Oe82ptJGdF-vIy1MIQk",
@@ -11,6 +12,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
+// Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export them so other pages can use them
+export { auth, db };
