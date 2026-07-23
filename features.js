be3dc675c424f5edfeacf24428b8051e406aba6c
anonc137 <anonc137@proton.me>
2026-07-23 21:14:10 +0100

    multiple changes

---
+++ features.js
@@ -0,0 +1,40 @@
+export const featureCategories = {
+
+    "Pokémon": [
+
+        {
+            id: "oldPokemon",
+            label: "Old Pokémon (2022 or earlier)"
+        },
+
+        {
+            id: "manyShinies",
+            label: "High amount of Shinies"
+        },
+
+        {
+            id: "shinyMewtwo",
+            label: "Shiny Mewtwo"
+        }
+
+    ],
+
+    "Progress": [
+
+        {
+            id: "level50",
+            label: "Level 50 Trainer"
+        }
+
+    ],
+
+    "Items": [
+
+        {
+            id: "masterBall",
+            label: "Master Ball"
+        }
+
+    ]
+
+};
